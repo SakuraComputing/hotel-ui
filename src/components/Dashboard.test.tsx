d9e6f78c49417c991e60866e51297d01148691ff
Ewan Bauld <ewan.bauld@gmail.com>
2023-11-02 20:57:00 +0000

    Add Dashboard Page

---
+++ src/components/Dashboard.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import DashBoardPage from './Dashboard';
+
+describe('/DashBoard', () => {
+    it('should render the dashboard page', () => {
+        render(<DashBoardPage />)
+        expect(screen.getByText('Dashboard')).toBeInTheDocument();
+
+    })
+})
