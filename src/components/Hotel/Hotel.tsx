30c751edae0e8582e649a0e20d7423cd28400fd1
Ewan Bauld <ewan.bauld@gmail.com>
2023-11-05 21:47:33 +0000

    Apply styling to hotel component part 1

--- src/components/Hotel/Hotel.tsx
+++ src/components/Hotel/Hotel.tsx
@@ -10,40 +10,42 @@ const Hotel: React.FC<IHotelProps> = ({hotel}) => {
 
     const { name, resort, rating, occupants, date, duration, departure, price, overview } = hotel;
 
-    const  [ isExpanded, setIsExpanded ] = useState(false);
+    const  [ isOverviewOpen, setOverviewOpen ] = useState(false);
 
-    const onClickOverview = () => {setIsExpanded(!isExpanded)};
+    const onClickOverview = () => {setOverviewOpen(!isOverviewOpen)};
 
-    const toggleString = isExpanded ? 'less' : 'more';
+    const toggleString = isOverviewOpen ? 'less' : 'more';
 
     return (
         <React.Fragment>
-            <aside className='hotel-container'>
-                <div className='img-container'>
-                    <div 
-                        className='overview-toggle' 
-                        role='button' 
-                        onClick={onClickOverview} 
-                        data-testid='overview-toggle'
-                    >
-                        <span>{`Read ${toggleString} about this hotel`}</span>
-                        <img 
-                            src={`/img/chevron-${isExpanded ? 'up' : 'down'}.png`} 
-                            alt={isExpanded ? 'overview__open' : 'overview__closed'} 
-                            className='icon'
-                        />
+            <aside className={isOverviewOpen ? 'hotel-container-expanded' : 'hotel-container'}>
+                <div className={isOverviewOpen ? 'top-container-expanded' : 'top-container-not-expanded'}>
+                    <div className='img-container'>
+                        <div 
+                            className='overview-toggle' 
+                            role='button' 
+                            onClick={onClickOverview} 
+                            data-testid='overview-toggle'
+                        >
+                            <span>{`Read ${toggleString} about this hotel`}</span>
+                            <img 
+                                src={`/img/chevron-${isOverviewOpen ? 'down' : 'up'}.png`} 
+                                alt={isOverviewOpen ? 'overview__open' : 'overview__closed'} 
+                                className='icon'
+                            />
+                        </div>
+                    </div>
+                    <div className='details-container'>
+                        <div>{name}</div>
+                        <div>{resort}</div>
+                        <div>{rating}</div>            
+                        <div>{occupants}</div>  
+                        <span><div>{formatDate(date)}</div><div>{duration}</div></span>                                  
+                        <div>{departure}</div>            
+                        <div>{price}</div>            
                     </div>
                 </div>
-                <div className='details-container'>
-                    <div>{name}</div>
-                    <div>{resort}</div>
-                    <div>{rating}</div>            
-                    <div>{occupants}</div>  
-                    <span><div>{formatDate(date)}</div><div>{duration}</div></span>                                  
-                    <div>{departure}</div>            
-                    <div>{price}</div>            
-                </div>
-                {isExpanded && <p>{overview}</p>}            
+                {isOverviewOpen && <p className='overview'>{overview}</p>}            
             </aside>
         </React.Fragment>
     )
